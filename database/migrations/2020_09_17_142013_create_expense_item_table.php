7674f7577795cf60baf090a78bc2a60b57af3bc6
Ivan <ivanminez28@gmail.com>
2020-09-17 22:49:13 +0800

    Created expense_item table

---
+++ database/migrations/2020_09_17_142013_create_expense_item_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpenseItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expense_item', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->date('date');
+            $table->decimal('amount', 10, 2);
+            $table->decimal('approved_amount', 10, 2);
+            $table->integer('submit_user_id');
+            $table->integer('expenses_id');
+            $table->integer('expenses_category_id');
+            $table->integer('asset_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expense_item');
+    }
+}
