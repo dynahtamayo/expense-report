801fb3b814edd61a6f383cd3fd97ee5aae1e23a8
MarkzMolina <mackymolina09@gmail.com>
2020-09-17 11:26:29 +0800

    My Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mark Style
                 </div>
 
                 <div class="links">
