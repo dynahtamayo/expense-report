38788265482d3017b6ae332043219853f3cc8f0c
Ivan <ivanminez28@gmail.com>
2020-10-08 22:49:44 +0800

    Database seeder for Expense Items

--- resources/views/expense-items/edit.blade.php
+++ resources/views/expense-items/edit.blade.php
@@ -15,48 +15,48 @@
 						<div class="row">
 			        		<div class="input-field col s6">	
 								<input placeholder="Description" id="description" type="text" name="description" value='{{ $expense_item->description }}' required>
-								<label for="description"></label>
+								<label for="description">Description</label>
 							</div>
 
 							<div class="input-field col s6">	
 								<input placeholder="Date" id="date" type="Date" name="date" value='{{ $expense_item->date }}' required>
-								<label for="date"></label>
+								<label for="date">Date</label>
 							</div>
 						</div>
 
 						<div class="row">
 			        		<div class="input-field col s6">
 								<input placeholder="Amount" id="amount" type="number" step="0.01" name="amount" value='{{ $expense_item->amount }}' required>
-								<label for="amount"></label>
+								<label for="amount">Amount</label>
 							</div>
 
 							<div class="input-field col s6">	
 								<input placeholder="Approved Amount" id="approved_amount" type="number" step="0.01" name="approved_amount" value='{{ $expense_item->approved_amount }}' required>
-								<label for="approved_amount"></label>
+								<label for="approved_amount">Approved Amount</label>
 							</div>
 						</div>
 
 						<div class="row">
 			        		<div class="input-field col s6">
 								<input placeholder="Submit User ID" id="submit_user_id" type="number" name="submit_user_id" value='{{ $expense_item->submit_user_id }}' required>
-								<label for="submit_user_id"></label>
+								<label for="submit_user_id">Submit User ID</label>
 							</div>
 
 							<div class="input-field col s6">	
 								<input placeholder="Expenses ID" id="expenses_id" type="number" name="expenses_id" value='{{ $expense_item->expenses_id }}' required>
-								<label for="expenses_id"></label>
+								<label for="expenses_id">Expenses ID</label>
 							</div>
 						</div>
 
 						<div class="row">
 			        		<div class="input-field col s6">
 								<input placeholder="Expenses Category ID" id="expenses_category_id" type="number" name="expenses_category_id" value='{{ $expense_item->expenses_category_id }}' required>
-								<label for="expenses_category_id"></label>
+								<label for="expenses_category_id">Expenses Category ID</label>
 							</div>
 
 							<div class="input-field col s6">	
 								<input placeholder="Asset ID" id="asset_id" type="number" name="asset_id" value='{{ $expense_item->asset_id }}' required>
-								<label for="asset_id"></label>
+								<label for="asset_id">Asset ID</label>
 							</div>
 						</div>
 
