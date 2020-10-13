1a511ecf32c0da1d9e4dd0afbcaedd79e78301dc
dynahtamayo <44790935+dynahtamayo@users.noreply.github.com>
2020-10-13 10:51:47 +0800

    TDD for users and departments

---
+++ tests/Feature/DepartmentsFeatureTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Department;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DepartmentsFeatureTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function show_all_departments(){
+        $departments = factory(Department::class, 5)->create();
+        $response = $this->get('/departments');
+        $response->assertStatus(200);
+
+    }
+
+    /** @test */
+    public function show_one_Department(){
+        $departments = factory(Department::class)->create();
+
+        $response = $this->get('/departments/' . $departments->id);
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function create_Department(){
+        $response = $this->get('/departments/create');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function store_Department(){
+        $departments = factory(Department::class)->raw();
+
+        $response = $this->post('/departments', $departments);
+
+        $this->assertDatabaseHas('departments', $departments);
+        $response->assertRedirect('/departments');
+    }
+
+     /** @test */
+    public function edit_departments(){
+        $departments = factory(Department::class)->create();
+
+        $response = $this->get('/departments' . $departments->id . '/edit');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function update_departments(){
+        $departments = factory(Department::class)->create();
+        $departments_updated_values = factory(Department::class)->raw([
+            'departments_id' => $departments->id
+        ]);
+
+        $response = $this->put('/departments/' . $departments->id, $departments_updated_values);
+
+        $this->assertDatabaseMissing('departments', [
+            "first_name" => $departments->first_name,
+            "last_name" => $departments->last_name,
+            "phone" => $departments->phone,
+            "email" => $departments->email,
+            "password" => $departments->password,
+            "department_id" => $departments->department_id
+        ]);
+
+        $this->assertDatabaseHas('departments', $departments_updated_values);
+        $response->assertRedirect('/departments');
+    }
+
+    /** @test */
+    public function delete_Department(){
+        $departments = factory(Department::class)->create();
+
+        $response = $this->delete('/departments/' . $departments->id);
+
+        $this->assertDeleted('departments', ["id" => $departments->id]);
+        $response->assertRedirect('/departments');
+    }
+}
---
+++ tests/Feature/UsersFeatureTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UsersFeatureTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function show_all_users(){
+        $users = factory(User::class, 10)->create();
+        $response = $this->get('/users');
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function show_one_user(){
+        $users = factory(User::class)->create();
+
+        $response = $this->get('/users/' . $users->id);
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function create_user(){
+        $response = $this->get('/users/create');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function store_user(){
+        $users = factory(User::class)->raw();
+
+        $response = $this->post('/users', $users);
+
+        $this->assertDatabaseHas('users', $users);
+        $response->assertRedirect('/users');
+    }
+
+     /** @test */
+    public function edit_users(){
+        $users = factory(User::class)->create();
+
+        $response = $this->get('/users' . $users->id . '/edit');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function update_users(){
+        $users = factory(User::class)->create();
+        $users_updated_values = factory(User::class)->raw([
+            'users_id' => $users->id
+        ]);
+
+        $response = $this->put('/users/' . $users->id, $users_updated_values);
+
+        $this->assertDatabaseMissing('users', [
+            "first_name" => $users->first_name,
+            "last_name" => $users->last_name,
+            "phone" => $users->phone,
+            "email" => $users->email,
+            "password" => $staff->password,
+            "department_id" => $users->department_id
+        ]);
+
+        $this->assertDatabaseHas('users', $users_updated_values);
+        $response->assertRedirect('/users');
+    }
+
+    /** @test */
+    public function delete_user(){
+        $users = factory(User::class)->create();
+
+        $response = $this->delete('/users/' . $users->id);
+
+        $this->assertDeleted('users', ["id" => $users->id]);
+        $response->assertRedirect('/users');
+    }
+}
