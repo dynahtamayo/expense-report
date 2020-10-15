790f218254789d78fb22ae9684b99daa72df16bb
dynahtamayo <44790935+dynahtamayo@users.noreply.github.com>
2020-10-15 21:40:52 +0800

    Added register view file

---
+++ resources/views/register.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+@section('content')
+	  <h4>Register </h4>		
+	    <form action="/register" method="POST">
+		@include('layouts.errors')
+		@csrf
+			      
+		<input placeholder="First Name" id="first_name" type="string" name="first_name" >
+		<label for="first_name"></label>
+			 
+		<input placeholder="Last Name" id="last_name" type="string" name="last_name" >
+		<label for="last_name"></label>
+		<br><br>
+		<input placeholder="Phone" id="phone" type="string" name="phone" >
+		<label for="phone"></label>
+		<br><br>
+		<input placeholder="Email" id="email" type="email" name="email" >
+		<label for="email"></label>
+		<br><br>
+		<input placeholder="Password" id="password" type="password" name="password" >
+		<label for="password"></label>
+		<br><br>
+		<input placeholder="DepartmentID" id="department_id" type="number" name="department_id" >
+		<label for="department_id"></label>
+		<br><br>
+		<button  type="submit" name="action">Submit <i>send</i></button>
+	</form>			
+
+@endsection
