cf9d7d0786bb20ca954fdc64450390965ce10e06
dynahtamayo <44790935+dynahtamayo@users.noreply.github.com>
2020-10-16 10:06:23 +0800

    Updated assets

---
+++ resources/views/assets/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container">
+	<h1>Assets</h1>
+	<a href="/expense-items/create" class="btn waves-effect waves-light green">Create Asset<i class="material-icons right">create</i></a> <br> <br>
+
+		<table class="striped">
+
+				<tr>
+					<th>ID</th>
+					<th>Description</th>
+				</tr>
+
+				<tr>
+					<td>
+						@foreach($assets as $asset)
+							<a href="/assets/{{$asset->id}}">{{$asset->id}}</a><br>
+						@endforeach
+					</td>
+					<td>
+						@foreach($assets as $asset)							
+							{{$asset->description}} <br>					
+						@endforeach
+					</td>
+				</tr>
+
+		</table> 
+
+			</div>
+
+@endsection
